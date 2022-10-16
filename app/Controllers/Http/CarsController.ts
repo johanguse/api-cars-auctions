7a7ba9514cda5657b9f4647c575952fd933b9007
Johan Guse <johanguse@gmail.com>
2022-10-16 14:55:05 -0300

    add error try catch on car controller

--- app/Controllers/Http/CarsController.ts
+++ app/Controllers/Http/CarsController.ts
@@ -2,14 +2,22 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Car from 'App/Models/Car'
 
 export default class CarsController {
-  public async index({ request }: HttpContextContract) {
-    const page = request.input('page', 1)
-    const limit = 10
+  public async index({ request, response }: HttpContextContract) {
+    try {
+      const page = request.input('page', 1)
+      const limit = 10
 
-    const cars = await Car.query().preload('bids').paginate(page, limit)
+      const cars = await Car.query().preload('bids').paginate(page, limit)
 
-    return {
-      data: cars,
+      return {
+        data: cars,
+      }
+    } catch (err) {
+      return response.status(err.status).send({
+        message: {
+          error: 'Something went wrong while find cars',
+        },
+      })
     }
   }
 }
